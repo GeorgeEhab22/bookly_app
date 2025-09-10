acd65afebb2f052122f9e21c57174295dc8525de
gerogeehab22 <lio10messi2215@gmail.com>
2025-09-10 21:03:05 +0300

    craete app styles

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const String logo = 'assets/images/logo.png';
   static const String harryPotterBook = 'assets/images/book.jpg';
+  static const String bestSellerBook = 'assets/images/bestSellerBook.jpg';
 }

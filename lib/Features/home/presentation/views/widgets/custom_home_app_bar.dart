03a8220ad8eabe7e9fc663dffd49a1aaf0a3a99a
gerogeehab22 <lio10messi2215@gmail.com>
2025-09-13 01:47:03 +0300

    create custom search text field

--- lib/Features/home/presentation/views/widgets/custom_home_app_bar.dart
+++ lib/Features/home/presentation/views/widgets/custom_home_app_bar.dart
@@ -1,26 +1,30 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomHomeAppBar extends StatelessWidget {
   const CustomHomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 24),
-      child: Row(
-        children: [
-          Image.asset(AssetsData.logo,height: 19.1,),
-          const Spacer(),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(FontAwesomeIcons.magnifyingGlass,size: 25,),
-          ),
-        ],
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 24),
+        child: Row(
+          children: [
+            Image.asset(AssetsData.logo, height: 19.1),
+            const Spacer(),
+            IconButton(
+              onPressed: () {
+                GoRouter.of(context).push(AppRouter.search);
+              },
+              icon: const Icon(FontAwesomeIcons.magnifyingGlass, size: 22),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
---
+++ lib/Features/search/presentation/views/widgets/custom_search_text_field.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomSearchTextFiels extends StatelessWidget {
+  const CustomSearchTextFiels({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey.shade300, width: 1),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        hintText: 'Search',
+        suffixIcon: IconButton(
+          onPressed: () {},
+          icon: const Opacity(
+            opacity: 0.8,
+            child: Icon(FontAwesomeIcons.magnifyingGlass, size: 22),
+          ),
+        ),
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+    );
+  }
+}

ab525b34981dc420a86b730395ea0aa83e0b6654
gerogeehab22 <lio10messi2215@gmail.com>
2025-09-13 14:45:53 +0300

    create Fetch Featured Books Use Case

---
+++ lib/Features/home/use_cases/fetch_features_books_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/features/home/domain/repositories/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    return await homeRepo.fetchFeaturedBooks();
+  }
+}

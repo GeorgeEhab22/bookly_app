bf2a3970655afa420ad693a9d71e4865bd629633
gerogeehab22 <lio10messi2215@gmail.com>
2025-09-17 18:26:58 +0300

    implement fetch newest books

--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -4,7 +4,6 @@ import 'package:bookly_app/features/home/data/models/book_model/book_model.dart'
 abstract class HomeRemoteDataSource {
   Future<List<BookModel>> fetchNewsetBooks();
   Future<List<BookModel>> fetchFeaturedBooks();
-  Future<List<BookModel>> fetchSimilarBooks({required String category});
 }
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
@@ -17,21 +16,19 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
      return books;
   }
 
-  List<BookModel> getBooksList(Map<String, dynamic> data) {
+ 
+
+  @override
+  Future<List<BookModel>> fetchNewsetBooks() async{
+    var data = await apiService.get(endpoint: 'volumes?Filtering=-ebooks&Sorting=newest&q=programming');
+     List<BookModel> books = getBooksList(data);
+     return books;
+  }
+}
+ List<BookModel> getBooksList(Map<String, dynamic> data) {
     List<BookModel> books = [];
     for(var book in data['items']){
       books.add(BookModel.fromJson(book));
     }
     return books;
   }
-
-  @override
-  Future<List<BookModel>> fetchSimilarBooks({required String category}) {
-    throw UnimplementedError();
-  }
-  
-  @override
-  Future<List<BookModel>> fetchNewsetBooks() {
-    throw UnimplementedError();
-  }
-}

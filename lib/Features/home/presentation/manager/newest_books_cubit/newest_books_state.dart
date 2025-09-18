72fa74a7afc39ada04e2f72fc10f773e26337175
gerogeehab22 <lio10messi2215@gmail.com>
2025-09-18 21:04:07 +0300

    implement cubit fetch newest books

---
+++ lib/Features/home/presentation/manager/newest_books_cubit/newest_books_state.dart
@@ -0,0 +1,16 @@
+part of 'newest_books_cubit.dart';
+
+abstract class NewestBooksState {}
+
+final class NewestBooksInitial extends NewestBooksState {}
+
+final class NewestBooksLoading extends NewestBooksState {}
+
+final class NewestBooksSuccess extends NewestBooksState {
+  final List<BookEntity> books;
+  NewestBooksSuccess(this.books);
+}
+final class NewestBooksFailure extends NewestBooksState {
+  final String errMessage;
+  NewestBooksFailure(this.errMessage);
+}
